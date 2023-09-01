b6869a8418a8328c640c881d2e7601fd86bef863
moalifaz <fazelcm2000@gmail.com>
2023-09-01 13:08:57 +0530

    Add

--- src/Components/Sidebar/Admins/AdminForm.jsx
+++ src/Components/Sidebar/Admins/AdminForm.jsx
@@ -3,7 +3,26 @@ import { Outlet } from 'react-router-dom'
 
 const AdminForm = () => {
   return (
-    <div>AdminForm
+    <div>
+      <form action="">
+        <input type="text" />
+      </form>
+
+       Name
+•        Email ID
+•        Password Generator
+•        Mobile Number
+•        Institutes (toggle)
+o        Add 
+o        Edit
+o        Delete
+•        Report Generations (toggle)
+o        ON / OFF
+        Submit Button 
+        Reset Button
+        Submit and add Button
+
+
         <Outlet/>
     </div>
   )
